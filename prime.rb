64dbca739f4069dda8ff113f74318b1ba01d9de3
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-09 14:22:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 def prime?(num)
-  return false if !number.integer
+  return false if !num.integer?
   return false if num < 2 
   return true if num == 2 
   (2...num-1).each {|int| return false if num % int == 0}

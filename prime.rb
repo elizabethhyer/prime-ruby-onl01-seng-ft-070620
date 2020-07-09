9b0a55771c1d575b13bf2c976adae93a263e9cbb
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-09 14:18:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,4 @@
 def prime?(num)
-  if num < 2
-    return false
-  else
-    (2...num-1).to_a.each do |n|
-      return false if num % n == 0
-    end
-  end
-end
-
-
-
-def prime?(num)
   return false if !number.integer
   return false if num < 2 
   return true if num == 2 
